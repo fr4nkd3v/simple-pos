574b553d90e2267f5f90af3c507c7f7ba793fc67
fr4nkd3v <frank.code31@gmail.com>
2026-01-08 21:33:29 -0500

    chore: add remove local data button

--- src/components/pages/report-page/report-page.tsx
+++ src/components/pages/report-page/report-page.tsx
@@ -1,11 +1,22 @@
-import { Header } from '@/components/shared';
+import { Button, Header } from '@/components/shared';
 
 export const ReportPage = () => {
   return (
     <div className='p-5'>
-      <Header title='Reporte' icon='graphLi' />
+      <Header
+        title='Reporte'
+        icon='graphLi'
+      />
 
       <p>Aqui se mostrará el reporte de ventas</p>
+
+      <Button
+        onClick={() => {
+          localStorage.removeItem('orders');
+        }}
+      >
+        remove local data
+      </Button>
     </div>
   );
 };
