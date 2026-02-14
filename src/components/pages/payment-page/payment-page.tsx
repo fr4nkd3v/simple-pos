d3921e802713f9f401eaa1350dbad3ea6d30f776
fr4nkd3v <frank.code31@gmail.com>
2026-02-14 09:47:52 -0500

    feat: make padding-bottom dinamic by payment control

--- src/components/pages/payment-page/payment-page.tsx
+++ src/components/pages/payment-page/payment-page.tsx
@@ -4,12 +4,39 @@ import {
   PaymentSummary,
   RegisterPaymentControl,
 } from './components';
+import { useEffect, useRef, useState } from 'react';
 
 export const PaymentPage = () => {
   const pageTitle = `Pagar cuenta`;
+  const controlRef = useRef<HTMLDivElement | null>(null);
+  const [controlHeight, setControlHeight] = useState(0);
+
+  useEffect(() => {
+    const element = controlRef.current;
+    if (!element) return;
+
+    setControlHeight(element.getBoundingClientRect().height);
+
+    const observer = new ResizeObserver((entries) => {
+      for (const entry of entries) {
+        if (controlHeight !== entry.contentRect.height) {
+          setControlHeight(entry.contentRect.height || 0);
+        }
+      }
+    });
+    observer.observe(element);
+
+    return () => observer.disconnect();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
-    <div className='flex flex-col gap-5 p-5 pb-48'>
+    <div
+      className='flex flex-col gap-5 p-5'
+      style={{
+        paddingBottom: controlHeight ? `${controlHeight + 20}px` : undefined,
+      }}
+    >
       <Header
         title={pageTitle}
         icon='walletMoneyLi'
@@ -29,7 +56,10 @@ export const PaymentPage = () => {
         </div>
       </div>
 
-      <div className='fixed bottom-register-payment-control-offset left-0 w-full'>
+      <div
+        ref={controlRef}
+        className='fixed bottom-register-payment-control-offset left-0 w-full'
+      >
         <RegisterPaymentControl />
       </div>
     </div>
